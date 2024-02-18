82b83a0eb18f348a08e1780963b59893f0d45f26
JIWON KIM <kkjim1324@naver.com>
2024-02-19 03:44:33 +0900

    add homework(day2)

---
+++ src/main/java/com/group/libraryapp/homework1/HomeWork2Controller.java
@@ -0,0 +1,47 @@
+package com.group.libraryapp.homework1;
+
+import com.group.libraryapp.homework1.request.CalculatorRequest;
+import com.group.libraryapp.homework1.request.SumRequest;
+import com.group.libraryapp.homework1.response.CalculatorResponse;
+import com.group.libraryapp.homework1.response.DayResponse;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+@RestController()
+@RequestMapping("/api/v1")
+public class HomeWork2Controller {
+
+    @GetMapping("/calc")
+    public CalculatorResponse calculate(CalculatorRequest calculatorRequest){
+        int add = calculatorRequest.getNum1() + calculatorRequest.getNum2();
+        int minus = calculatorRequest.getNum1() - calculatorRequest.getNum2();
+        int multiply = calculatorRequest.getNum1() * calculatorRequest.getNum2();
+
+        CalculatorResponse calculatorResponse = new CalculatorResponse(add, minus, multiply);
+
+        return calculatorResponse;
+    }
+
+    @GetMapping("/day-of-the-week")
+    public DayResponse dauOfWeek(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date ){
+        DayResponse dayResponse = new DayResponse(date.getDayOfWeek().getDisplayName(TextStyle.SHORT, Locale.US));
+
+        return dayResponse;
+    }
+
+    @PostMapping("/sum")
+    public int sum(@RequestBody SumRequest sumRequest){
+        var requestList = sumRequest.getNumbers();
+        Integer response = 0;
+
+        for(int i = 0; i < requestList.size(); i++){
+            response += requestList.get(i);
+        }
+
+        return response;
+    }
+}
